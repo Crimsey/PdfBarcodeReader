d1eabdf74f179dddc2a9b1304664ea97e8b1ab03
Jakub Sokół <jakub.sokol.staz@webchili.onmicrosoft.com>
2021-07-16 14:30:19 +0200

    doin OA documentation...

--- src/Controller/ExtractPdfBarcodeController.php
+++ src/Controller/ExtractPdfBarcodeController.php
@@ -5,17 +5,65 @@
 namespace App\Controller;
 
 //use App\Controller\AbstractAPIController;
-use Swagger\Annotations as SWG;
+//use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
 
 class ExtractPdfBarcodeController //extends AbstractAPIController
 {
     /**
      * Extract the barcode from PDF.
      *
-     * @Route("/extractpdfbarcode", methods=["GET"])
+     * @Route("/api/doc/extractpdfbarcode", methods={"GET"})
      *
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns barcode/s from PDF"
+     *)
+     * @OA\Parameter(
+     *     name="operation_id",
+     *     in="query",
+     *     type="integer",
+     *     format="int64",
+     *     description="Single operation of extracting Barcode from PDF",
+     *     required="true"
+     * )
+     * @OA\Parameter(
+     *     name="pdf_file",
+     *     in="header",
+     *     type="object",
+     *     description="File in pdf format",
+     *     required="true"
+     * )
+     * @OA\Parameter(
+     *     name="jpeg_file",
+     *     in="header",
+     *     type="object",
+     *     description="File in jpeg format",
+     *     required="true"
+     * )
+     * @OA\Parameter(
+     *     name="counter",
+     *     in="header",
+     *     type="integer",
+     *     description="Counter of barcodes on single page",
+     *     required="true"
+     * )
+     * @OA\Parameter(
+     *     name="barcode_type",
+     *     in="header",
+     *     type="integer",
+     *     description="Type of barcode",
+     *     required="true"
+     * )
+     * @OA\Parameter(
+     *     name="barcode",
+     *     in="header",
+     *     type="string",
+     *     description="Barcode in string format",
+     *     required="true"
+     * )
      */
     public function test(): Response
     {
