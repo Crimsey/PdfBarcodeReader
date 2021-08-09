027205e117bbee706f862a265a185c5fd5f9ad63
Jakub Sokół <jakub.sokol.staz@webchili.onmicrosoft.com>
2021-08-09 12:34:49 +0200

    Handle the case when zbarimg Process returns messange - no barcodes found in file

--- src/Service/GetBarcode.php
+++ src/Service/GetBarcode.php
@@ -13,20 +13,26 @@ namespace App\Service;
             $process = new Process(['zbarimg', $imagefile->getRealPath()]);
 
             $process->run();
+            $barcode = $process->getOutput();
+            $barcode_error = $process->getErrorOutput();
+
+            if(strpos($barcode_error,"scanned 0 barcode symbols") != false) {
 
             if (!$process->isSuccessful()) {
                 throw new ProcessFailedException($process);
             }
-            $barcode = $process->getOutput();
-            $barcode = rtrim($barcode, "\n\r\t\v\0");
-            $pieces = explode("\n", $barcode);
-            $newArray = [] ?? '';
-            foreach ($pieces as $lineNum => $line) {
-                $line = trim($line);
-                list($key, $value) = explode(':', $line, 2);
-                $newArray[$key][] = $value;
+                $process->disableOutput()->clearOutput();
+
+                $barcode = rtrim($barcode, "\n\r\t\v\0");
+                $pieces = explode("\n", $barcode);
+                $newArray = [] ?? '';
+                foreach ($pieces as $lineNum => $line) {
+                    $line = trim($line);
+                    list($key, $value) = explode(':', $line, 2);
+                    $newArray[$key][] = $value;
+                }
             }
 
-            return $newArray;
+            return $newArray ?? ['No barcodes found'];
         }
     }
